ccf83ae303c00bc87344339b6474b26d80ca80ad
Day <874288011@qq.com>
2018-11-30 13:28:56 +0800

    update(install.sh)：更新安装脚本

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+# pcre -> rewrite 功能
+# openssl -> ssl 功能
 yum install gcc gcc-c++ glibc make zlib-devel pcre-devel openssl-devel -y
 wget -c http://nginx.org/download/nginx-1.12.1.tar.gz
 tar -xzf nginx-1.12.1.tar.gz
 cd  nginx-1.12.1
-./configure --prefix=/usr/local/nginx/ --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-http_realip_module  --with-http_mp4_module --with-http_flv_module
+./configure --prefix=/usr/local/nginx/ --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-http_realip_module
 make
 make install
